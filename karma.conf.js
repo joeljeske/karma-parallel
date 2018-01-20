780d105cd1447a0d68c33a125c414f4acb86e904
Joel Jeske <joeljeske14@gmail.com>
2018-01-19 23:18:20 -0600

    Ensure our test complies

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'parallel'],
+    frameworks: ['parallel', 'jasmine'],
 
 
     // list of files / patterns to load in the browser
