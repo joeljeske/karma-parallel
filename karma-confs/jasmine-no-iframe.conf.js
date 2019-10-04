7b319693b8059677a7fbf3a8ff542515810129a3
Devin Wilson <devin.wilson@readytalk.com>
2019-10-04 12:15:49 -0600

    Fix shardIndexInfoMap id lookup when karma config useIframe is false

---
+++ karma-confs/jasmine-no-iframe.conf.js
@@ -0,0 +1,7 @@
+
+module.exports = function(config) {
+  require('./base.conf.js')(config);
+  config.plugins.push('karma-jasmine');
+  config.frameworks.push('jasmine');
+  config.client.useIframe = false;
+};
